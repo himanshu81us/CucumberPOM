b3c3b74117bb034f709abc4966550be9e5bb8696
Himanshu Patel <Himanshu.Patel@playtech.com>
2023-06-02 09:25:29 +0100

    New Code

--- pages/basepage.js
+++ pages/basepage.js
@@ -15,6 +15,30 @@ class basepage{
     go_to_url(site){
       driver.get(site);
     }
+
+    async createUser(){
+
+      var user = "Q";
+          
+        var n = 8 ;
+        var possible = "abcdefghijklmnopqrstuvwxyz";
+
+        for( var i=0; i < n; i++ )
+            user += possible.charAt(Math.floor(Math.random() * possible.length));
+            user= user[0].toUpperCase() + user.substring(1);
+
+          return user;
+
+     }
+
+     async takeScreenShot(){
+
+      return await driver.takeScreenshot().then(function(referenceScreenShot) {
+        
+        world.attach(referenceScreenShot, "base64:image/png");
+
+      });
+    }
     
 }
 
